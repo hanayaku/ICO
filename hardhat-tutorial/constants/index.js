2ac8bbb155ae4fd39917da280d56339fcc36db64
hanayaku <fafasavefafa@163.com>
2022-12-04 17:35:59 +0800

    初始化

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xCAdffA37fd9f8FE3D284a6475B74C6D34D28BE22";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
